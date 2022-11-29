785749b2abcf7486e8ce31ed9e054e5b2e99accf
Crowdbotics <team@crowdbotics.com>
2022-11-29 16:34:02 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "smart_pump_37737",
+  url: "https://smart_pump_37737.botics.co",
+  api: "https://smart_pump_37737.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
